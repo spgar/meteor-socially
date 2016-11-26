635e03e59a3cbcd4da00b2eb0f12fbb2a67e12a1
Steve Gargolinski <steve.gargolinski@gmail.com>
2016-11-26 13:43:06 -0500

    Adds party detail skeleton

---
+++ client/imports/app/parties/party-details.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs/Subscription';
+
+import 'rxjs/add/operator/map';
+
+import { Parties } from '../../../../both/collections/parties.collection';
+import { Party } from '../../../../both/models/party.model';
+ 
+import template from './party-details.component.html';
+ 
+@Component({
+  selector: 'party-details',
+  template
+})
+
+export class PartyDetailsComponent implements OnInit, OnDestroy {
+  partyId: string;
+  paramsSub: Subscription;
+  party: Party;
+
+  constructor(
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit() {
+    this.paramsSub = this.route.params
+      .map(params => params['partyId'])
+      .subscribe(partyId => {
+        this.partyId = partyId;
+        this.party = Parties.findOne(this.partyId);
+      });
+  }
+
+  ngOnDestroy() {
+    this.paramsSub.unsubscribe();
+  }
+}
